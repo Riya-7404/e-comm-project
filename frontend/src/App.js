164d508bd2c5c7aa4ca33710ebe17f570881489b
Riya Gaikwad <gaikwadriya138@gmail.com>
2025-09-07 15:40:28 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
@@ -7,12 +7,14 @@ import InfoPage from "./pages/InfoPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/info" element={<InfoPage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/info" element={<InfoPage />} />
+      </Routes>
+    </Router>
   );
 }
 
